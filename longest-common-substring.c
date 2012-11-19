24c308604ca254fe8858198b0663666b5de986b7
Adrian Sitterle <adrian.sitterle@gmail.com>
2012-11-18 19:37:43 -0700

    adding longest-common-sequence.c

---
+++ longest-common-substring.c
@@ -0,0 +1,71 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* lcs(char* s1, char* s2)
+{
+	int s1_l = strlen(s1), s2_l = strlen(s2);
+	int i, s1_i, s2_i, longest = 0;
+
+	char* set = (char*)malloc(sizeof(char) * ((s1_l < s2_l)?s1_l:s2_l));
+	memset(set, ' ', strlen(set));
+
+	// initialize 2d array
+	int** l = (int**)malloc((s1_l + 1) * sizeof(int*));
+	for (i = 0; i < s1_l + 1; i++)
+		l[i] = (int*)malloc((s2_l + 1) * sizeof(int));
+	
+	// initialize all array fields to 0
+	for (s1_i = 0; s1_i < s1_l; s1_i++)
+		for (s2_i = 0; s2_i < s2_l; s2_i++)
+			l[s1_i][s2_i] = 0;
+			
+	// loop through all letters
+	for (s1_i = 0; s1_i < s1_l; s1_i++)
+		for (s2_i = 0; s2_i < s2_l; s2_i++)
+		{
+			// if characters match
+			if (s1[s1_i] == s2[s2_i])
+			{
+
+				// calculate the incremented length for current sequence
+				int v = l[s1_i][s2_i] + 1;
+
+				// save the incremented length in south-east cell
+				l[s1_i+1][s2_i+1] = v;
+
+				// if new length is longest thus far
+				if (v > longest)
+				{
+					// set that to be our new longest length
+					longest = v;
+
+					// and clear the set
+					printf(">%s\n", set);
+					memset(set, ' ', strlen(set));
+				}	
+
+				// if new length equals to the longest length
+				if (v == longest)
+				{
+					// copy the char set to the return set
+					for (i = 0; i < longest; i++)
+						set[i] = s1[s1_i-longest+i+1];
+				}
+			}
+		}
+
+
+
+	return set;
+}
+
+
+int main()
+{
+	char* s1 = "lolingipopabcder";
+	char* s2 = "philolingogisabcdt";
+
+	printf("%s\n", lcs(s1, s2));
+}
