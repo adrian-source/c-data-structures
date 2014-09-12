f84fb18c590a6d85b02a67b5c7a35ab7b169d43f
adrian-source <adrian.sitterle@gmail.com>
2014-09-12 08:48:23 -0600

    Update binary-tree.c
    fixing a bug in delete() where a node with two children would be deleted as if it only had one. Adding more test cases.

--- binary-tree.c
+++ binary-tree.c
@@ -1,78 +1,56 @@
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 struct node{
+	int key;
 	struct node* lower;
 	struct node* higher;
-
-	int key;
 };
 
-int height(struct node* tree)
+
+void insert(int key, struct node** tree)
 {
-	if (tree == 0)
+
+	if (*tree == NULL)
+	{
+		*tree = (struct node*)malloc(sizeof(struct node));
+		(*tree)->key = key;
+	}
+	else if (key < (*tree)->key)
+		insert(key, &(*tree)->lower);
+	else if (key > (*tree)->key)
+		insert(key, &(*tree)->higher);
+	// else
+	//	key already exists in tree
+
+}
+
+void print(struct node** tree)
+{
+	if ((*tree)->lower != NULL)
+		print(&(*tree)->lower);
+
+	printf("%d ", (*tree)->key);
+
+	if ((*tree)->higher != NULL)
+		print(&(*tree)->higher);
+}
+
+int height(struct node** tree)
+{
+	if (*tree == NULL)
 		return 0;
 	else
 	{
-		int lower = 1 + height(tree->lower);
-		int higher = 1 + height(tree->higher);
-		return (lower > higher) ? lower : higher;
+		int l = 1 + height(&(*tree)->lower);
+		int h = 1 + height(&(*tree)->higher);
+		return ((l < h)?h:l);
 	}
 }
-
-int factor(struct node* tree)
-{
-	int l = height(tree->lower);
-	int r = height(tree->higher);
-	return l - r;
-}
-
-void insert (int key, struct node** tree)
-{
-
-	if (*tree == 0)
-	{
-		printf("i inserting... %d\n", key);
-		*tree = (struct node*)malloc(sizeof(struct node));
-		(*tree)->key = key;
-		(*tree)->lower = 0;
-		(*tree)->higher = 0;
-	}
-	else if (key < (*tree)->key)
-	{
-		printf("i lower...\n");
-		insert(key, &(*tree)->lower);
-	}
-	else if (key > (*tree)->key)
-	{
-		printf("i higher...\n");
-		insert(key, &(*tree)->higher);
-	}
-}
-
-
-void print (struct node** tree)
-{
-	
-	if ((*tree)->lower != 0)
-	{
-		printf("/\n");
-		print(&(*tree)->lower);
-	}
-	printf("%d \n", (*tree)->key);
-
-	if ((*tree)->higher != 0)
-	{	
-		printf("\\\n");
-		print(&(*tree)->higher);
-	}
-}
-
 
 struct node** find_largest(struct node** tree)
 {
-	if ((*tree)->higher != 0)
+	if ((*tree)->higher != NULL)
 		return find_largest(&(*tree)->higher);
 	else
 		return tree;
@@ -80,86 +58,70 @@ struct node** find_largest(struct node** tree)
 
 void delete(int key, struct node** tree)
 {
-	if (*tree != 0)
+	if (*tree == NULL)
 	{
-		if (key == (*tree)->key)
+		return; // shouldn't get here
+	}
+	else
+	{
+		if ((*tree)->key == key)
 		{
-			printf("d found it!\n");
-			if ((*tree)->lower != 0 && (*tree)->higher != 0)
+			if ((*tree)->lower == NULL && (*tree)->higher == NULL)
+			// deleting a leaf node
 			{
-				printf("d has two children...\n");
-				struct node** replacement = find_largest(&(*tree)->lower);
-				(*tree)->key = (*replacement)->key;
-				if ((*replacement)->lower != 0)
-					(*replacement) = (*replacement)->lower;
-
-				free(*replacement);
-				(*replacement) = NULL;
-			}
-			else if ((*tree)->lower != 0 || (*tree)->higher != 0)
-			{
-				printf("d deleting node with one child... %d\n", (*tree)->key);
-				if ((*tree)->lower != 0)
-					*tree = (*tree)->lower;
-				else if ((*tree)->higher != 0)
-					*tree = (*tree)->higher;
-			}
-			else if ((*tree)->lower == 0 && (*tree)->higher == 0)
-			{
-				printf("d deleting leaf... %d\n", (*tree)->key);
 				free(*tree);
 				*tree = NULL;
 			}
+			else if ((*tree)->lower != NULL && (*tree)->higher != NULL)
+			// deleting node with two children
+			{
+				struct node** replacement = find_largest(&(*tree)->lower);
+				(*tree)->key = (*replacement)->key;
+				if ((*replacement)->lower != NULL)
+					(*replacement) = (*replacement)->lower;
+			}
+			else if ((*tree)->lower != NULL || (*tree)->higher != NULL)
+			// deleting node with one child
+			{
+				if ((*tree)->lower != NULL)
+					*tree = (*tree)->lower;
+				else if ((*tree)->higher != NULL)
+					*tree = (*tree)->higher;	
+			}
+
 		}
 		else if (key < (*tree)->key)
-		{
-			delete(key, &(*tree)->lower);
-		}
+			delete(key, &(*tree)->lower);	
 		else if (key > (*tree)->key)
-		{
-			delete(key, &(*tree)->higher);
-		}
+			delete(key, &(*tree)->higher);	
+			
 	}
+
+
 }
-
-
-
-
 
 int main()
 {
-	struct node* tree;
+	struct node* tree = (struct node*)malloc(sizeof(struct node));
 	tree->key = 50;
-	tree->lower = 0;
-	tree->higher = 0;
-
 	insert(40, &tree);
-	insert(45, &tree);
-	insert(30, &tree);
-	insert(35, &tree);
-	insert(60, &tree);
-
-
-	print(&tree);
-
-	/*
-	tree->key = 50;
-	tree->lower = 0;
-	tree->higher = 0;
-
 	insert(2, &tree);
 	insert(10, &tree);
 	insert(100, &tree);
 	insert(51, &tree);
 	insert(49, &tree);
 	insert(0, &tree);
-
-	delete(50, &tree);
-
-	printf("height... %d\n", height(tree));
-
-	printf("printing...\n");
+	insert(-1, &tree);
 
 	print(&tree);
-	*/
+	printf("\n");
+
+	printf("tree height %d \n", height(&tree));
+
+	printf("largest element %d \n", (*(find_largest(&tree)))->key); 
+
+	delete(-1, &tree);
+	print(&tree);
+	printf("\n");
+	printf("tree height %d \n", height(&tree));
 }
