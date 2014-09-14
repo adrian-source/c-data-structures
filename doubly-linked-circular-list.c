c616f75cc0a3f5a8560200dc8bd74756dc343b6d
adrian-source <adrian.sitterle@gmail.com>
2014-09-13 20:02:34 -0600

    fix for adding element at front and back of dlst

--- doubly-linked-circular-list.c
+++ doubly-linked-circular-list.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -6,94 +5,115 @@ struct node{
 	struct node* prev;
 	struct node* next;
 	int data;
-}*last;
+};
 
-add(int data)
+struct node* last;
+
+void add(int data)
 {
-	printf("adding %d...\n", data);
-
-	struct node* tmp;
-	struct node* curr;
-	tmp = (struct node*)malloc(sizeof(struct node));
-	tmp->data = data;
-
+	struct node* newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	
 	if (last == NULL)
+	// no elements present
 	{
-		last = tmp;
-		last->data = data;
-		last->next = last;
-		last->prev = last;
+		newnode->next = newnode;
+		newnode->prev = newnode;
+		last = newnode;
 	}
+	else if (last->next == last)
+	// one element present
+	{
+		last->next = newnode;
+		last->prev = newnode;
+		newnode->prev = last;
+		newnode->next = last;
+		
+		if (last->data < newnode->data)
+			last = newnode;
+	}
+	// more than one element present
 	else
 	{
-		// there is onle one element
-		if (last->next == last)
+		struct node* curr = last->next;
+		while(curr != NULL)
 		{
-
-			last->next = tmp;
-			last->prev = tmp;
-			tmp->next = last;
-			tmp->prev = last;
-
-			if (last->data < tmp->data)
-				last = tmp;
-			
-		}
-		else
-		{
-			curr = last->next;
-			while(curr != NULL)
+			if (curr == last->next && data < curr->data)
+			// newnode needs to be places before first element
 			{
-				if (tmp->data > curr->data && tmp->data < curr->next->data)
-				{
-					tmp->next = curr->next;
-					curr->next->prev = tmp;
-					tmp->prev = curr;
-					curr->next = tmp;
-					break;
-				}
-				else if (curr->data < tmp->data)
-				{
-					curr = curr->next;
-				}
+				last->next = newnode;
+				newnode->prev = last;
+				curr->prev = newnode;
+				newnode->next = curr;
+				break;
+			}
+			else if (curr == last && curr->data < data)
+			// newnode needs to be placed after the last element
+			{
+				last->next->prev = newnode;
+				newnode->next = last->next;
+				newnode->prev = last;
+				last->next = newnode;
+				last = newnode;	
+				break;
+			}
+			else if (curr->data < data && data < curr->next->data)
+			{
+				newnode->next = curr->next;
+				newnode->next->prev = newnode;
+				newnode->prev = curr;
+				curr->next = newnode;				
+				break;
+			}
+			else if (curr->data == data)
+			{
+				return; // element already exists
 			}
 
+			curr = curr->next;
+	
 		}
-
 	}
+	
 }
 
 void print()
 {
-	if (last->next == last)
-	{
-		printf("one item %d\n", last->data);
-	}
+	if (last == NULL)
+		printf("empty dlst");
 	else
 	{
-		struct node* curr;
-		curr = last->next;
+		struct node* curr = last->next;
 		while (curr != NULL)
 		{
-			printf("%d\n", curr->data);
-			curr = curr->next;
+			printf("%d ", curr->data);
 			if (curr == last)
 			{
-				printf("%d last \n", curr->data);
+				printf("\n");
 				break;
 			}
+			else
+				curr = curr->next;
 		}
+
 	}
 }
 
 int main()
 {
 	last = NULL;
-	add(3);
-	add(5);
-	add(4);
-	add(100);
-	add(1);
 
+	add(3);
 	print();
+	add(5);
+	print();
+	add(4);
+	print();
+	add(4);  // adding in the middle
+	print();
+	add(1);	 // adding in the front
+	print(); 
+	add(10);
+	print(); // adding at the end 
 }
+
